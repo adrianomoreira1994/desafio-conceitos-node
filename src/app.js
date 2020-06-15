96124701f882b3cf069f2ca03330c81048ca4c63
Adriano <carlosadrianosmoreira@gmail.com>
2020-06-15 14:34:45 -0300

    finalizada criacao de repositorios

--- src/app.js
+++ src/app.js
@@ -1,25 +1,51 @@
 const express = require("express");
 const cors = require("cors");
 
-// const { uuid } = require("uuidv4");
+const { uuid } = require("uuidv4");
 
 const app = express();
-
+// { id: "uuid", title: 'Desafio Node.js', url: 'http://github.com/...', techs: ["Node.js", "..."], likes: 0 };
 app.use(express.json());
 app.use(cors());
 
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
-  // TODO
+  try {
+    return response.json(repositories);
+  } catch (error) {
+    console.log(error.message);
+  }
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  try {
+    const { title, url, techs, likes } = request.body;
+
+    const repository = {
+      id: uuid(),
+      title,
+      url,
+      techs,
+      likes,
+    };
+
+    repositories.push(repository);
+    return response.status(201).send(repository);
+  } catch (error) {
+    return response.status(500).send("Request Error");
+  }
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+  const repositoryIndex = repositories.findIndex(
+    (repository) => repository.id === id
+  );
+
+  if (repositoryIndex < 0) {
+    return response.status(400).send({ error: "Repository is not found" });
+  }
 });
 
 app.delete("/repositories/:id", (request, response) => {
